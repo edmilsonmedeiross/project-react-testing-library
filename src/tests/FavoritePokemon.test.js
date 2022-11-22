905083004e46e2c5c519861986ec138f7e3f878e
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-11-22 20:47:05 -0300

    requisito 3 finalizado

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/dom';
+/* import userEvent from '@testing-library/user-event'; */
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos;', () => {
+    // acessar
+    renderWithRouter(<FavoritePokemon />);
+    const textNoFavourite = screen.getByText(/no favorite pokémon found/i);
+    // agir
+
+    // aferir
+    expect(textNoFavourite).toBeInTheDocument();
+  });
+});

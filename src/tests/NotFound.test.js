359cc98db9db8dc2c7d83ee7e58d52d75d874b80
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2022-11-22 21:11:59 -0300

    requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/dom';
+/* import userEvent from '@testing-library/user-event'; */
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found;', () => {
+    // acessar
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading', { name: /page requested not found/i });
+    // agir
+
+    // aferir
+    expect(title).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    // acessar
+    renderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByRole('img');
+    // agir
+
+    // aferir
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
